5e2ec2a08bef295fa4de35d6ecda7f1ed2aee66b
mariangelaimperato <mariangelaimperato@gmail.com>
2025-09-25 14:38:49 +0200

    footer icon

--- src/compontents/organisms/Footer.jsx
+++ src/compontents/organisms/Footer.jsx
@@ -1,15 +1,30 @@
 // src/components/organisms/Footer.jsx
 import React from 'react';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="app-footer">
       <p>&copy; {new Date().getFullYear()} Meal App. Tutti i diritti riservati.</p>
+      
       <div className="footer-links">
         <a href="#">Privacy</a>
         <a href="#">Termini di servizio</a>
-        <a href="mailto:mealapp@gmail.com">Contatti</a> {/* Link mailto */}
+        <a href="mailto:mealapp@gmail.com">Contatti</a>
+      </div>
+
+      {/* Icone social */}
+      <div className="footer-social">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
       </div>
     </footer>
   );
